ef1eb64858c624029668483058949c8395f79993
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-04-15 00:50:12 +1000

    add docker-compose for development

---
+++ .docker/conf/django-dev.sh
@@ -0,0 +1,7 @@
+DJANGODIR=/application
+cd ${DJANGODIR}
+export PYTHONPATH=${DJANGODIR}:${PYTHONPATH}
+if [ -n "$NORELOAD" ]; then
+    PARAMS="--noreload"
+fi
+python manage.py runserver 0.0.0.0:8000 ${PARAMS}
--- backend/default_settings.py
+++ backend/default_settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)^ia#9$43p+2uqdz8%lanh19hx)mx%cqrw!(4_t(z48&n92gdt'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+DEBUG = os.getenv('DJANGO_DEBUG')
+ALLOWED_HOSTS = [os.getenv('DJANGO_ALLOWED_HOSTS'), ]
+CSRF_TRUSTED_ORIGINS = [os.getenv('DJANGO_CSRF_TRUSTED_ORIGINS'), ]
 
 # Application definition
 
@@ -70,16 +67,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = dict(default=dict(
+    ENGINE='django.db.backends.postgresql_psycopg2', NAME=os.getenv('DATABASE_NAME'),
+    USER=os.getenv('POSTGRES_USER'), PASSWORD=os.getenv('POSTGRES_PASSWORD'), HOST='database',
+    PORT=os.getenv('DATABASE_PORT', '5432')
+))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
