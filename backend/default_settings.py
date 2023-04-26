d2737dde28b8567a8466b283ff3772009cb4cb3c
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-04-26 15:58:54 +1000

    add static root

--- backend/default_settings.py
+++ backend/default_settings.py
@@ -111,6 +111,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = 'media'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
