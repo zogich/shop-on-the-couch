ca04cf8f4ed15fea66cb823839fb888a8d2470bc
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-04-30 20:58:36 +1000

    check co/cd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Stage has been deployed. Fuck Sergey Semenov.
         </p>
         <a
           className="App-link"
