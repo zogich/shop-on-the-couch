80e11a6ea9c433c4de9136294091ff98dc0adc6c
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-05-10 20:41:57 +1000

    fix vue app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    outDir: '../dist'
+  },
+  base: 'static'
 })
