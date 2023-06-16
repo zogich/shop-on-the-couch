0827034f6979949ccc936c7ca23b2c085ee1a628
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-06-16 21:51:41 +1000

    add base store

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 
 </script>
 
 <template>
-  <header>Хедер</header>
+  <header>
+    Хедер
+  </header>
   <RouterView />
   <footer>
     Подвал
   </footer>
-
 </template>
 
 <style scoped>
+header {
+
+
+}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
-
+import store from './store/index'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router).use(store)
 
 app.mount('#app')
