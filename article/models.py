c392ef224f6ddc9a4b56573da9009b582cba428d
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-06-19 17:26:37 +1000

    switch product to article

--- article/models.py
+++ article/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
 
-class Product(models.Model):
+
+class Article(models.Model):
     name = models.CharField(max_length=100)
-    price = models.FloatField()
+    publication_date = models.DateField()
+    text = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
---
+++ article/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from article.serializers import ArticleSerializer, CategorySerializer
+from article.models import Article, Category
+
+
+class ArticleViewSet(ModelViewSet):
+
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+
+class CategoryViewSet(ModelViewSet):
+
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
