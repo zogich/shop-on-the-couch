0f0bf91e933a250db50fa4bb66c5ae107202f39d
TCKACHIKSIS <71429999+zogich@users.noreply.github.com>
2023-06-22 14:41:42 +1000

    add represent name of category

--- article/serializers.py
+++ article/serializers.py
@@ -13,3 +13,8 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+    def to_representation(self, instance):
+        representation = super(ArticleSerializer, self).to_representation(instance)
+        representation['category'] = instance.category.name
+        return representation
